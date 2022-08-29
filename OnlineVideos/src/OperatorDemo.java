9ea35aa5308b59d35a086676f7ff1e33f547804b
SreedharGundu <47936316+SreedharGundu@users.noreply.github.com>
2022-08-29 10:01:25 +0530

    first Commit

---
+++ OnlineVideos/src/OperatorDemo.java
@@ -0,0 +1,15 @@
+
+public class OperatorDemo {
+
+	public static void main(String[] args) 
+	{
+		int m = 4;
+		int n = 5;
+		
+		n += m;   // n = n + m
+		System.out.println(n);
+		System.out.println(m);
+
+	}
+
+}
